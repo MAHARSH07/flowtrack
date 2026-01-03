8a94bea2e145286ae6cf46cafafb79d65b243d3c
MAHARSH07 <142198509+MAHARSH07@users.noreply.github.com>
2026-01-03 11:24:40 +0530

    Add new allowed origin for CORS

--- app/main.py
+++ app/main.py
@@ -28,7 +28,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://flowtrack-orcin.vercel.app"
+        "https://flowtrack-orcin.vercel.app",
+        "https://flowtrack-20xba89fj-maharsh07s-projects.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
